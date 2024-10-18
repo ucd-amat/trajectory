4d96b6e6da70999dd19ffd1856b8dbe32f7c2587
Nicholas Rusali <149333463+Nick-M-R@users.noreply.github.com>
2024-10-18 16:39:09 -0700

    added some stuff for landing

--- main.py
+++ main.py
@@ -116,9 +116,9 @@ def takeoff(V_rot,theta_climb_deg,rotation_rate_degps,gtow,RPM = 10000):
 
         dx = np.array([
             x[1], #x_dot
-            (Thrust*np.cos(theta)-D*np.cos(theta)-L*np.sin(theta))/(gtow/32.2),
+            (Thrust*np.cos(theta)-D*np.cos(alpha)-L*np.sin(alpha))/(gtow/32.2),
             x[3], #y_dot
-            (Thrust*np.sin(theta)-D*np.sin(theta)+L*np.cos(theta))/(gtow/32.2) - 32.2,
+            (Thrust*np.sin(theta)-D*np.sin(alpha)+L*np.cos(alpha))/(gtow/32.2) - 32.2,
         ])
 
         if dx[3] < 0 and x[2] <= 0: #y_2dot < 0
@@ -147,8 +147,117 @@ def takeoff(V_rot,theta_climb_deg,rotation_rate_degps,gtow,RPM = 10000):
 
     return history
         
+if False:
+    history = takeoff(50,15,10,15,8000)
 
-history = takeoff(50,15,10,15,8000)
+    for var in ["x","y","u","v","theta","alpha","CL","CD","Power"]:
+        if var == "theta" or var == "alpha":
+            plt.plot(history["t"],np.array(history[var])*(180/np.pi),label=var)
+        else:
+            plt.plot(history["t"],history[var],label=var)
+
+    plt.xlabel("t")
+    plt.legend()
+    plt.grid()
+
+    plt.figure()
+
+    for var in ["t","y","u","v","theta","alpha","CL","CD","Power"]:
+        if var == "theta" or var == "alpha":
+            plt.plot(history["x"],np.array(history[var])*(180/np.pi),label=var)
+        else:
+            plt.plot(history["x"],history[var],label=var)
+
+    plt.xlabel("x")
+    plt.legend()
+    plt.grid()
+
+    print("Takeoff Energy (Wh)", np.trapz(history["Power"],x=history["t"])/3600)
+
+    plt.show()
+
+def landing(V_init,theta_glide,alt_init,gtow,RPM = 10000):
+    x = np.zeros(4) #initialize x_dot
+    x[1] = V_init
+    x[2] = alt_init
+    mu = 0.03
+    t = 0
+    dt = 0.01
+    cl_max = 1.4
+    theta = 0 #radians
+    theta0 = 2*(np.pi/180) #radius
+    prop_performance = parse_APC_data(file_name,x = "RPM", y = "V_fps")[0]
+
+    history = dict(
+        t = [],
+        x = [],
+        y = [],
+        u = [],
+        v = [],
+        theta = [],
+        alpha = [],
+        CL = [],
+        CD = [],
+        Power = [],
+
+    )
+
+    # while x[2] < 35 and x[0] < 1000: #ft
+    while x[0] < 1000 and x[1] >= 0: #ft
+        t += dt
+
+        normal_airspeed_fps = ((x[1]*np.cos(theta_glide))**2 + (x[3]*np.sin(theta_glide))**2)**(0.5)
+        airspeed_fps = ((x[1])**2 + (x[3])**2)**(0.5)
+
+        prop_data = prop_performance(RPM,normal_airspeed_fps)
+        Thrust = prop_data["Thrust_lbf"]*0
+        Power = prop_data["PWR_W"]*0
+        qS = ((1/2)*rho*airspeed_fps**2*S)
+        alpha = theta - np.nan_to_num(np.arctan(x[3]/x[1]))
+        CL = cl_max*np.sin(2*np.pi*(alpha + theta0)/cl_max) #assume linear lift slope
+        CD = dragCoeff_estimate(CL)
+        D = CD*qS
+        L = CL*qS
+
+
+
+        dx = np.array([
+            x[1], #x_dot
+            (Thrust*np.cos(theta)-D*np.cos(alpha)-L*np.sin(alpha))/(gtow/32.2), # +x force
+            x[3], #y_dot
+            (Thrust*np.sin(theta)-D*np.sin(alpha)+L*np.cos(alpha))/(alpha-gtow/32.2) - 32.2, # +y force
+        ])
+
+
+        if dx[3] < 0 and x[2] <= 0: #y_2dot < 0
+            x[3] = 0 #y_dot
+            dx[1] += -np.abs(mu*dx[3])
+            dx[2] = 0 #y_dot
+            dx[3] = 0 #y_2dot
+
+        x += dx*dt
+
+        # print("Time (s)", t)
+        # print("Theta",theta*(180/np.pi))
+        # print("Alpha",alpha*(180/np.pi))
+        # print("dx",dx)
+        # print("x",x)
+        # print("CL",CL)
+
+        history["t"].append(t)
+        history["x"].append(x[0])
+        history["y"].append(x[2])
+        history["u"].append(x[1])
+        history["v"].append(x[3])
+        history["theta"].append(theta_glide)
+        history["alpha"].append(alpha)
+        history["CL"].append(CL)
+        history["CD"].append(CD)
+        history["Power"].append(Power)
+
+    return history
+
+history = landing(50,0,50,15)
 
 for var in ["x","y","u","v","theta","alpha","CL","CD","Power"]:
     if var == "theta" or var == "alpha":
@@ -168,6 +277,8 @@ for var in ["t","y","u","v","theta","alpha","CL","CD","Power"]:
     else:
         plt.plot(history["x"],history[var],label=var)
 
+plt.plot([0,history["x"][-1]],[1.4,1.4],label="C_L max") # hypothetical CL_max = 1.4 straight line
+
 plt.xlabel("x")
 plt.legend()
 plt.grid()
@@ -175,9 +286,6 @@ plt.grid()
 print("Takeoff Energy (Wh)", np.trapz(history["Power"],x=history["t"])/3600)
 
 plt.show()
-
-
-    
 
 
 
